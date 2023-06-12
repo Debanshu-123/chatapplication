6610c453ad6632b4802e2b9048820d2ccb017cd5
Debanshu <bhoidebanshu123@gmail.com>
2023-06-12 20:37:23 +0530

    chatapplication

---
+++ src/components/userProfile/UserProfile.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./userProfile.css";
+
+import logo from "./../../images/Deb.jpeg";
+
+export default class UserProfile extends Component {
+  toggleInfo = (e) => {
+    e.target.parentNode.classList.toggle("open");
+  };
+  render() {
+    return (
+      <div className="main__userprofile">
+        <div className="profile__card user__profile__image">
+          <div className="profile__image">
+             
+            <img src={logo}></img>
+          </div>
+          <h4>Debanshu Ranjan Bhoi</h4>
+          <p>CEO & Founder at Bhoichat</p>
+        </div>
+        <div className="profile__card">
+          <div className="card__header" onClick={this.toggleInfo}>
+            <h4>Information</h4>
+            <i className="fa fa-angle-down"></i>
+          </div>
+          <div className="card__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            ultrices urna a imperdiet egestas. Donec in magna quis ligula
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
